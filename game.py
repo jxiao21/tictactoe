bff187fe41d7db60f82f2403cbf30e9b1ce4c570
justxiao <justxiao@umich.edu>
2022-07-21 00:54:42 -0400

    add unbeatable AI

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, Goat
 import time
 import math
 
@@ -101,6 +101,6 @@ def play(game, x_player, o_player, print_game=True):
 
 if __name__ == '__main__':
     x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
+    o_player = Goat('O')
     t = tictactoe()
     play(t, x_player, o_player, print_game = True)
--- player.py
+++ player.py
@@ -44,3 +44,41 @@ class Goat(Player):
     def get_move(self, game):
         if len(game.available_moves()) == 9:
             square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)['position']
+
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else 
+                    -1 * (state.num_empty_squares() + 1)}
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+
+        for possible_move in state.available_moves():
+            # make move, try spot
+            state.make_move(possible_move, player)
+            # recurse with minimax to simulate game after making move
+            sim_score = self.minimax(state, other_player)
+            # undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move
+            # update dictionaries if necessary
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+
+        return best
